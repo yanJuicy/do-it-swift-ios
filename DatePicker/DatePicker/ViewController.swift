616338ff089db464fd634f77a5688de158bb81d9
yanJuicy <hanjub3@gmail.com>
2023-07-20 00:58:43 +0900

    데이트 피커 앱

---
+++ DatePicker/DatePicker/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by YANJU1CY 🇰🇷 on 2023/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text = "선택시간: \(formatter.string(from: datePickerView.date))"
+    }
+    
+    @objc func updateTime() {
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: \(formatter.string(from: date as Date))"
+    }
+}
+
