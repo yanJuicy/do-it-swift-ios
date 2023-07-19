59f2f051578089b159b1944faabd6ecc4532c514
yanJuicy <hanjub3@gmail.com>
2023-07-19 22:36:21 +0900

    이미지 뷰 앱

---
+++ ImageView/ImageView/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by YANJU1CY 🇰🇷 on 2023/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var btnResize: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        imgView.image = imgOn
+    }
+
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if (isZoom) {
+            newWidth = imgView.frame.width / scale
+            newHeight = imgView.frame.height / scale
+            btnResize.setTitle("확대", for: .normal)
+        } else {
+            newWidth = imgView.frame.width * scale
+            newHeight = imgView.frame.height * scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    
+    @IBAction func swithImagOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
+    }
+}
+
