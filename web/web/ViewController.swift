d490cff6eaa236aca7026b636d429eab650ba43c
yanJuicy <hanjub3@gmail.com>
2023-07-21 01:28:43 +0900

    피커 뷰 앱

---
+++ web/web/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  web
+//
+//  Created by yanju on 2023/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
