c2d932a80fc05458ac859c2fe8f20810731425c2
yanJuicy <hanjub3@gmail.com>
2023-07-22 01:19:31 +0900

    페이지 컨트롤

---
+++ PageControl/PageControl/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by yanju on 2023/07/21.
+//
+
+import UIKit
+
+var images = ["01.png", "02.png", "03.png", "04.png", "05.png", "06.png"]
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.green
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        
+        imgView.image = UIImage(named: images[0])
+    }
+
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+
+}
+
